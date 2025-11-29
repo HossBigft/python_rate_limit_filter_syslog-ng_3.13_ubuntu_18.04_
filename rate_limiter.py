73c1ece2c041868cd740b16d47eac1224ac33d28
Maksim Gluwenko <maksim_gluwenko@epam.com>
2025-11-29 18:57:14 +0500

    Fix all dropping on burst

--- rate_limiter.py
+++ rate_limiter.py
@@ -1,8 +1,22 @@
 #!/usr/bin/env python3
 import sys
+import time
 
-output_file_path = "/tmp/rate_limit_passed.log"
+output_file_path = "/var/log/rate_limit_test"
+MAX_LINES_PER_SECOND = 10
+
+last_time = time.time()
+lines_this_second = 0
 
 with open(output_file_path, "a") as f_out:
     for line in sys.stdin:
-        f_out.write(line)
+        current_time = time.time()
+        if current_time - last_time >= 1:
+            last_time = current_time
+            lines_this_second = 0
+
+        if lines_this_second < MAX_LINES_PER_SECOND:
+            f_out.write(line)
+            f_out.flush()
+            lines_this_second += 1
+        # Excess lines are silently dropped
