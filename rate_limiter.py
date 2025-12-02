b60cdb06d953c417de9b484a2609f8d7d8df72c9
Maksim Gluwenko <maksim_gluwenko@epam.com>
2025-12-02 11:49:47 +0500

    Add functionality to pass parameter from syslog

--- rate_limiter.py
+++ rate_limiter.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python3
 import time
 
-class RateLimiter:
+class RateLimiter(object):
     def __init__(self):
         self.max_lines_per_second = 10
         self.last_time = time.time()
         self.lines_this_second = 0
     
     def init(self, options):
+        if "max_logs_per_second" in options:
+            self.max_lines_per_second = int(options["max_logs_per_second"])
         return True
     
     def deinit(self):
@@ -24,4 +26,4 @@ class RateLimiter:
             self.lines_this_second += 1
             return True  
         
-        return False  
+        return False
