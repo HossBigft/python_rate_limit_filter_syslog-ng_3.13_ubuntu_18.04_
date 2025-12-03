753592b516421cbbed092ef111db7810493fcfe1
Maksim Gluwenko <maksim_gluwenko@epam.com>
2025-12-03 12:39:09 +0500

    Add rate limiting based on site domain name

--- rate_limiter.py
+++ rate_limiter.py
@@ -1,29 +1,44 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 import time
+
+class DomainTracker:
+    def __init__(self):
+        self.counter = 0  # type: int
+        self.first_sighting = time.time()  # type: float
+
+    def increment(self):
+        self.counter += 1
+
+    def reset(self):
+        self.counter = 0
+        self.first_sighting = time.time()
+
 
 class RateLimiter(object):
     def __init__(self):
         self.max_lines_per_second = 10
-        self.last_time = time.time()
-        self.lines_this_second = 0
-    
+        self.domains = {}  # type: dict[str, DomainTracker]
+
     def init(self, options):
         if "max_logs_per_second" in options:
             self.max_lines_per_second = int(options["max_logs_per_second"])
         return True
-    
+
     def deinit(self):
         pass
-    
+
     def parse(self, log_message):
-        current_time = time.time()
-        
-        if current_time - self.last_time >= 1:
-            self.last_time = current_time
-            self.lines_this_second = 0
-        
-        if self.lines_this_second < self.max_lines_per_second:
-            self.lines_this_second += 1
-            return True  
-        
-        return False
+        current_time = time.time()  # type: float
+        site_domain = log_message["PROGRAM"]  # type: str
+
+        if site_domain not in self.domains.keys():
+            self.domains[site_domain] = DomainTracker()
+
+        tracker = self.domains[site_domain]
+
+        if current_time - tracker.first_sighting >= 1:
+            tracker.reset()
+
+        tracker.increment()
+
+        return tracker.counter <= self.max_lines_per_second
