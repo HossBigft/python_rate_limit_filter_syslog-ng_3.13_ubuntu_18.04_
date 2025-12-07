3a8d11910ba0a544873f3d52e9802ff4d1ee2794
Maksim Gluwenko <maksim_gluwenko@epam.com>
2025-12-07 22:47:06 +0500

    Move  refill logic into parse function to speed up function

--- rate_limiter.py
+++ rate_limiter.py
@@ -2,41 +2,21 @@
 import time
 
 class TokenBucket:
-    def __init__(self, rate=10000):
+    __slots__ = ('rate', 'tokens', 'last_sighting')
+    def __init__(self, last_sighting, rate=10000):
         self.rate = rate   # type: int
         self.tokens = 0.0  # type: float
-        self.last_sighting = time.time()  # type: float
+        self.last_sighting =  last_sighting# type: float
            
-    def refill_tokens(self):
-        now = time.time()
-        elapsed = now - self.last_sighting  
-        
-        if elapsed >= 1:  
-            num_refills = int(elapsed)  
-            tokens_to_add = num_refills * self.rate  
-        
-            self.tokens += tokens_to_add
-            self.tokens = min(self.rate, self.tokens)
-            
-            self.last_sighting = now
-    
-    
-    def has_tokens(self, amount):
-        return amount <= self.tokens
-
-    def consume(self, amount):
-        self.refill_tokens()
-        if self.has_tokens(amount):
-            self.tokens -= amount
-            return True
-
-        return False
 
 
 class RateLimiter(object):
+    __slots__ = ('max_lines_per_second', 'domains')
+    
     def __init__(self):
         self.max_lines_per_second = 10
         self.domains = {}  # type: dict[str, TokenBucket]
+        
     def init(self, options):
         if "max_logs_per_second" in options:
             self.max_lines_per_second = int(options["max_logs_per_second"])
@@ -44,9 +24,20 @@ class RateLimiter(object):
 
 
     def parse(self, log_message):
+        now = time.time()
         site_domain = log_message["PROGRAM"]  # type: str
         if site_domain not in self.domains:
-            self.domains[site_domain] = TokenBucket(rate=self.max_lines_per_second)
+            self.domains[site_domain] = TokenBucket(rate=self.max_lines_per_second, last_sighting=now)
         bucket = self.domains[site_domain]  # type: TokenBucket
-        return bucket.consume(1)
+        elapsed = now - bucket.last_sighting
+        
+        if elapsed >= 1.0:
+            bucket.tokens = min(bucket.rate, bucket.tokens + int(elapsed) * bucket.rate)
+            bucket.last_sighting = now
+        
+        if bucket.tokens >= 1.0:
+            bucket.tokens -= 1.0
+            return True
+        
+        return False
     
