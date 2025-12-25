7225d088469f82124dda69798509a7f3ece90257
Maksim Gluwenko <maksim_gluwenko@epam.com>
2025-12-25 13:20:53 +0500

    Add cleanup of dormant domains

--- rate_limiter.py
+++ rate_limiter.py
@@ -11,20 +11,29 @@ class TokenBucket:
 
 
 class RateLimiter(object):
-    __slots__ = ('max_lines_per_second', 'domains')
+    __slots__ = ('max_lines_per_second', 'domains', 'last_cleanup')
     
     def __init__(self):
         self.max_lines_per_second = 10
         self.domains = {}  # type: dict[str, TokenBucket]
+        self.last_cleanup = time.time() # type: float
         
     def init(self, options):
         if "max_logs_per_second" in options:
             self.max_lines_per_second = int(options["max_logs_per_second"])
         return True
 
-
+    def cleanup_old_domains(self, now, max_age_s=3):
+        domains_to_remove = [
+            domain for domain, bucket in self.domains.items()
+            if now - bucket.last_sighting > max_age_s
+        ]
+        for domain in domains_to_remove:
+            del self.domains[domain]
     def parse(self, log_message):
+        CLEANUP_PERIOD_S = 300 # type: int 
         now = time.time()
+
         site_domain = log_message["PROGRAM"]  # type: str
         if site_domain not in self.domains:
             self.domains[site_domain] = TokenBucket(rate=self.max_lines_per_second, last_sighting=now)
@@ -34,7 +43,9 @@ class RateLimiter(object):
         if elapsed >= 1.0:
             bucket.tokens = min(bucket.rate, bucket.tokens + int(elapsed) * bucket.rate)
             bucket.last_sighting = now
-        
+            if now - self.last_cleanup > CLEANUP_PERIOD_S:
+                self.cleanup_old_domains(now)
+                self.last_cleanup = now
         if bucket.tokens >= 1.0:
             bucket.tokens -= 1.0
             return True
